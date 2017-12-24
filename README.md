5b237716701a1333eed9b6f438968823f8d17bb9
nintichance <mintidance@gmail.com>
2017-12-24 06:38:18 -0500

    Portfolio designed with place holder images and text

---
+++ README.md
@@ -0,0 +1,3 @@
+[Image from TJ Holowaychuck](/Users/nintichance/ga_wdi/portfolio/images/tj-holowaychuk-113931.jpg)
+
+[icons](https://www.flaticon.com/)
---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+$('nav > a').addClass('animated pulse')
+
+let personality = ['placeholder','outgoing', 'hard working', 'inquisitive', 'bubbly.']
+let clicked = 0
+$('article > h1').on('click', this, function(){
+clicked +=1
+$(this).append('<article><p>' + personality[clicked] + '</p></article>')
+if (clicked === 4){
+    $(this).off()
+}
+})
+
+})
