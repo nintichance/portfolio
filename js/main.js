bad72fbbf98629554c24cea507958d12f5ae0fab
nintichance <mintidance@gmail.com>
2017-12-24 07:09:57 -0500

    Added some fx

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
 $('nav > a').addClass('animated pulse')
-
+$('.section > img').addClass('animated pulse')
 let personality = ['placeholder','outgoing', 'hard working', 'inquisitive', 'bubbly.']
 let clicked = 0
 $('article > h1').on('click', this, function(){
